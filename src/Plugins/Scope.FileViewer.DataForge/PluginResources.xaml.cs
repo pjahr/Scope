8ef413c69beaaecdbc6153be8796c6cbb6901138
Paul Jahr <paul.jahr@web.de>
2020-06-09 12:10:10 +0200

    Added exported resources for DF plugin.

---
+++ src/Plugins/Scope.FileViewer.DataForge/PluginResources.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Scope.Interfaces;
+using System;
+using System.ComponentModel.Composition;
+using System.Windows;
+
+namespace Scope.FileViewer.DataForge
+{
+  [Export]
+  public class PluginResources : ResourceDictionary, IResourceDictionary
+  {
+    public Uri Uri { get; } = ExternalResourceDictionaryUri.Create<PluginResources>();
+  }
+}
