0e6ef54f044e19cb542cf40deb22197f9baf04b0
pjahr <paul.jahr@web.de>
2021-07-06 07:50:54 +0200

    Added test data.

---
+++ src/Plugins/Scope.Plugins.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using Moq;
+using Scope.FileViewer.DataForge.Models;
+using Scope.Interfaces;
+using System;
+using System.IO;
+using Xunit;
+using File = System.IO.File;
+
+namespace Scope.Plugins.Tests
+{
+  public class DataForgeFileFacts
+  {
+    private DataForgeFile _sut;
+    private IMessageQueue _messageQueue;
+    private IProgress<ProgressReport> _progress;
+
+    [Fact]
+    public void It_deserializes_the_dcb_contents()
+    {
+      _messageQueue = Mock.Of<IMessageQueue>();
+      _progress = Mock.Of<IProgress<ProgressReport>>();
+
+      using var r = new BinaryReader(File.OpenRead(@"TestData/game.dcb"));
+      _sut = new DataForgeFile(r,_messageQueue, _progress);
+
+      
+    }
+  }
+}
