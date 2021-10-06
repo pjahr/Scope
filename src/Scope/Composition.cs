6965f2cc6889d20b3187af36f3c16ee906d0335c
pjahr <paul.jahr@web.de>
2021-10-06 22:07:10 +0200

    Replacing Castle.Windsor with Lamar.

--- src/Scope/Composition.cs
+++ src/Scope/Composition.cs
@@ -3,53 +3,60 @@ using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.IO.Abstractions;
 using System.Linq;
-using Castle.MicroKernel.Registration;
-using Castle.MicroKernel.Resolvers.SpecializedResolvers;
-using Castle.Windsor;
 using Scope.Interfaces;
 using Scope.Views;
+using Lamar;
 
 namespace Scope
 {
   internal class Composition : IDisposable
   {
-    private readonly WindsorContainer _container;
+    private readonly Container _container;
 
     public Composition()
     {
-      var container = new WindsorContainer();
+      var registry = new ServiceRegistry();
 
-      // enable injection of multiple services with the same interface
-      container.Kernel.Resolver.AddSubResolver(new CollectionResolver(container.Kernel));
+      registry.Scan(_ =>
+      {
+        _.TheCallingAssembly();
+        _.AssembliesFromPath("Plugins");
+        _.AddAllTypesOf<AppWindow>();
+        _.RegisterConcreteTypesAgainstTheFirstInterface();
+        _.WithDefaultConventions();
+      });
 
-      // special register: 'real' file sytem
-      container.Register((ComponentRegistration<object>)Component.For(typeof(IFileSystem))
-                                                                 .ImplementedBy(typeof(FileSystem))
-                                                                 .LifeStyle.Singleton);
+      _container = new Container(registry);
 
-      // register built-in app object graph
-      container.Register((BasedOnDescriptor)Classes.FromAssemblyContaining<Composition>()
-                                                   .IncludeNonPublicTypes()
-                                                   .Where(t => t.GetCustomAttributes(false)
-                                                                .Any(a => a is ExportAttribute))
-                                                   .WithServiceSelf()
-                                                   .WithServiceAllInterfaces()
-                                                   .LifestyleSingleton());
-      // register plugins
-      var registrations =
-        Classes.FromAssemblyInDirectory(new AssemblyFilter(@"Plugins"))
-                                 .IncludeNonPublicTypes()
-                                 .Pick()
-                                 .If(t => t.GetCustomAttributes(false)
-                                           .Any(a => a is ExportAttribute))
-                                 .WithServiceAllInterfaces()
-                                 .LifestyleSingleton();
-      container.Register(registrations);
+      //// enable injection of multiple services with the same interface
+      //container.Kernel.Resolver.AddSubResolver(new CollectionResolver(container.Kernel));
 
-      _container = container;
+      //// special register: 'real' file sytem
+      //container.Register((ComponentRegistration<object>)Component.For(typeof(IFileSystem))
+      //                                                           .ImplementedBy(typeof(FileSystem))
+      //                                                           .LifeStyle.Singleton);
 
-      PluginResources = container.ResolveAll<IResourceDictionary>();
-      MainWindow = _container.Resolve<AppWindow>();
+      //// register built-in app object graph
+      //container.Register((BasedOnDescriptor)Classes.FromAssemblyContaining<Composition>()
+      //                                             .IncludeNonPublicTypes()
+      //                                             .Where(t => t.GetCustomAttributes(false)
+      //                                                          .Any(a => a is ExportAttribute))
+      //                                             .WithServiceSelf()
+      //                                             .WithServiceAllInterfaces()
+      //                                             .LifestyleSingleton());
+      //// register plugins
+      //var registrations =
+      //  Classes.FromAssemblyInDirectory(new AssemblyFilter(@"Plugins"))
+      //                           .IncludeNonPublicTypes()
+      //                           .Pick()
+      //                           .If(t => t.GetCustomAttributes(false)
+      //                                     .Any(a => a is ExportAttribute))
+      //                           .WithServiceAllInterfaces()
+      //                           .LifestyleSingleton();
+      //container.Register(registrations);      
+
+      PluginResources = _container.GetAllInstances<IResourceDictionary>();
+      MainWindow = _container.GetInstance<AppWindow>();
     }
 
     public AppWindow MainWindow { get; }
